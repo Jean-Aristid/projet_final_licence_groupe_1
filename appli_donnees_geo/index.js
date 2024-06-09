2556e064342aacd670ff9fb570e6767f92c38aae
Donogbo N'guessan <aristidezakari24@gmail.com>
2024-06-10 00:20:17 +0200

    amélioration du projet avec VUEJS

---
+++ appli_donnees_geo/index.js
@@ -0,0 +1,74 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        location: '',
+        currentLocation: { name: '', lat: '', lon: '' },
+        map: null,
+        marker: null
+    },
+    mounted() {
+        this.initialiserMap();
+    },
+    methods: {
+        initialiserMap() {
+            this.map = L.map('map').setView([48.8566, 2.3522], 13);
+
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            }).addTo(this.map);
+
+            this.map.on('click', this.on_map_click);
+        },
+        chercher(){
+            fetch(`https://nominatim.openstreetmap.org/search?format=json&q=${this.location}`)
+                .then(response => response.json())
+                .then(data => {
+                    if (data && data.length > 0) {
+                        var lat = data[0].lat;
+                        var lon = data[0].lon;
+                        var name = data[0].display_name;
+                        this.map.setView([lat, lon], 13);
+                        if (this.marker) {
+                            this.map.removeLayer(this.marker);
+                        }
+                        this.marker = L.marker([lat, lon]).addTo(this.map)
+                            .bindPopup('<b>' + name + '</b>')
+                            .openPopup();
+                        this.currentLocation = { name: name, lat: lat, lon: lon };
+                    } else {
+                        alert("Lieu non trouvé");
+                    }
+                })
+                .catch(error => console.error('Erreur:', error));
+        },
+        on_map_click(e) {
+            var lat = e.latlng.lat;
+            var lon = e.latlng.lng;
+            if (this.marker) {
+                this.map.removeLayer(this.marker);
+            }
+            this.marker = L.marker([lat, lon]).addTo(this.map)
+                .bindPopup('<b>Coordonnées :</b><br>Latitude: ' + lat + '<br>Longitude: ' + lon)
+                .openPopup();
+            this.currentLocation = { name: 'Coordonnées cliquées', lat: lat, lon: lon };
+        },
+        imprimer() {
+            if (this.currentLocation.name === '' && this.currentLocation.lat === '' && this.currentLocation.lon === '') {
+                alert('Aucune information de lieu disponible pour imprimer.');
+                return;
+            }
+
+            var content = `Nom de la ville : ${this.currentLocation.name}\nLatitude : ${this.currentLocation.lat}\nLongitude : ${this.currentLocation.lon}`;
+            var blob = new Blob([content], { type: 'text/plain' });
+            var url = URL.createObjectURL(blob);
+            var a = document.createElement('a');
+            a.href = url;
+            a.download = 'location_' + this.currentLocation.name + '.txt';
+            document.body.appendChild(a);
+            a.click();
+            document.body.removeChild(a);
+            URL.revokeObjectURL(url);
+        }
+    }
+});
+
